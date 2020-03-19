3b04025bd272e4f07e4880d1dfab3ec2880e87ea
ddhorstman <davidzilla27@gmail.com>
2020-03-19 19:21:55 -0400

    Initial set-up

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
-  const { register, errors, handleSubmit, reset } = useForm({
+  const { register, errors, handleSubmit } = useForm({
     mode: "onBlur"
   });
   const onSubmit = data => {
