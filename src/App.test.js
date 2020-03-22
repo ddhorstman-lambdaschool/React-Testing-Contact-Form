0e52b1fd36d004f8f12daf7d09e8ed6a7a515907
ddhorstman <davidzilla27@gmail.com>
2020-03-22 16:24:02 -0400

    Finished Kirkby's modified project!

--- src/App.test.js
+++ src/App.test.js
@@ -27,3 +27,26 @@ test("contains a message field", () => {
   const { getByPlaceholderText } = render(<ContactForm />);
   expect(getByPlaceholderText(/message/i));
 });
+
+test("contains a click button", () => {
+  const Form = render(<ContactForm />);
+
+  const clickButton = Form.getByText(/click me/i);
+  expect(clickButton);
+
+});
+
+test("increments the button when clicked", () => {
+  const Form = render(<ContactForm />);
+  const clickButton = Form.getByText(/click me/i);
+  expect(clickButton.innerHTML).toContain("0");
+  act(() => {
+    fireEvent.click(clickButton);
+  });
+  expect(clickButton.innerHTML).toContain("1");
+  act(() => {
+    fireEvent.click(clickButton);
+  });
+  expect(clickButton.innerHTML).toContain("2");
+  
+});
